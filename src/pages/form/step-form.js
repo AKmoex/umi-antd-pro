8715ce760a6053247ad5b4834ec53f57cdad8332
AKmoex <akmoex@gmail.com>
2020-09-14 22:45:26 +0800

    step-form

--- src/pages/form/step-form.js
+++ src/pages/form/step-form.js
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import {
   Form,
   Input,
-  Checkbox,
-  DatePicker,
-  Tooltip,
-  InputNumber,
+  Descriptions,
   Button,
   Select,
   Steps,
+  Alert,
+  Divider,
 } from 'antd';
 
 import BaseInfo from '../components/BaseInfo.js';
 
 class List extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      current: 0,
-    };
-  }
-  next() {
-    const current = this.state.current + 1;
-    this.setState({ current });
-  }
+  state = {
+    current: 0,
+    initialValues: {
+      payAccount: 'antAlipay',
+      receiveType: 'alipay',
+      receiveAccount: 'test@example.com',
+      receiveName: 'Alex',
+      payAmount: 500,
+    },
+  };
+  formRef1 = React.createRef();
+  //formRef2 = React.createRef();
 
-  prev() {
-    const current = this.state.current - 1;
-    this.setState({ current });
+  componentDidMount() {
+    //异步获取默认值给state
+    //this.formRef.current.setFieldsValue(this.state.ii);
   }
 
   render() {
@@ -39,8 +40,6 @@ class List extends Component {
         span: 9,
       },
     };
-    const { Step } = Steps;
-    const { current } = this.state;
 
     return (
       <div>
@@ -58,68 +57,111 @@ class List extends Component {
           }}
         >
           <Steps
-            current={current}
+            current={this.state.current}
             style={{ maxWidth: '750px', margin: '10px auto' }}
           >
-            <Step title="填写转账信息" />
-            <Step title="确认转账信息" />
-            <Step title="完成" />
+            <Steps.Step title="填写转账信息" />
+            <Steps.Step title="确认转账信息" />
+            <Steps.Step title="完成" />
           </Steps>
+
           <div className="steps-content">
-            {current === 0 && (
-              <Form {...layout}>
-                <Form.Item label="付款账户">
-                  <Select defaultValue="antAlipay">
-                    <Option value="antAlipay">ant-design@alipay.com</Option>
+            {this.state.current === 0 && (
+              <Form
+                {...layout}
+                ref={this.formRef1}
+                initialValues={this.state.initialValues}
+              >
+                <Form.Item name="payAccount" label="付款账户">
+                  <Select>
+                    <Select.Option value="antAlipay">
+                      ant-design@alipay.com
+                    </Select.Option>
                   </Select>
                 </Form.Item>
                 <Form.Item label="收款账户">
                   <Input.Group compact>
-                    <Form.Item name="receive-accounts" noStyle>
-                      <Select defaultValue="alipay">
-                        <Option value="alipay">支付宝</Option>
-                        <Option value="bank">中国银行</Option>
+                    <Form.Item name="receiveType" noStyle>
+                      <Select>
+                        <Select.Option value="alipay">支付宝</Select.Option>
+                        <Select.Option value="bank">中国银行</Select.Option>
                       </Select>
                     </Form.Item>
                     <Form.Item
-                      name="account"
+                      name="receiveAccount"
                       noStyle
-                      rules={[{ required: true, message: '输入收款人账户' }]}
+                      rules={[
+                        { required: true, message: '输入收款人账户' },
+                        { type: 'email', message: '账户名应为邮箱格式' },
+                      ]}
                     >
-                      <Input
-                        style={{ width: '50%' }}
-                        defaultValue="example@test.com"
-                      />
+                      <Input style={{ width: '50%' }} />
                     </Form.Item>
                   </Input.Group>
                 </Form.Item>
-                <Form.Item label="收款人姓名">
-                  <Input defaultValue="Alex"></Input>
+                <Form.Item
+                  name="receiveName"
+                  label="收款人姓名"
+                  rules={[{ required: true, message: '请输入收款人姓名' }]}
+                >
+                  <Input></Input>
                 </Form.Item>
                 <Form.Item
+                  name="payAmount"
                   label="转账金额"
                   rules={[{ required: true, message: '请输入转账金额' }]}
                 >
-                  <Input prefix="￥" defaultValue="500"></Input>
+                  <Input prefix="￥"></Input>
+                </Form.Item>
+              </Form>
+            )}
+            {this.state.current === 1 && (
+              <Form {...layout}>
+                <Alert
+                  message="确认转账后，资金将直接打入对方账户，无法退回。"
+                  type="info"
+                  showIcon
+                  closable
+                  className="alert-style"
+                />
+
+                <Descriptions>
+                  <Descriptions.Item label="付款账户">
+                    {this.state.initialValues.payAccount}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="收款账户">
+                    {this.state.initialValues.receiveAccount}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="收款人姓名">
+                    {this.state.initialValues.receiveName}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="转账金额">
+                    {this.state.initialValues.payAmount}
+                  </Descriptions.Item>
+                </Descriptions>
+                <Divider />
+                <Form.Item
+                  name="payPassword"
+                  label="支付密码"
+                  rules={[{ required: true, message: '请输入支付密码' }]}
+                >
+                  <Input type="password"></Input>
                 </Form.Item>
               </Form>
             )}
           </div>
           <div className="steps-action">
-            {current < 1 && (
+            {this.state.current < 1 && (
               <Button type="primary" onClick={() => this.next()}>
                 下一步
               </Button>
             )}
-            {current === 1 && (
-              <Button
-                type="primary"
-                onClick={() => message.success('Processing complete!')}
-              >
+            {this.state.current === 1 && (
+              <Button type="primary" onClick={this.handleClick}>
                 提交
               </Button>
             )}
-            {current > 0 && (
+            {this.state.current > 0 && (
               <Button style={{ margin: '0 8px' }} onClick={() => this.prev()}>
                 上一步
               </Button>
@@ -128,6 +170,37 @@ class List extends Component {
         </div>
       </div>
     );
+  }
+
+  next() {
+    //先更新一下state
+    // !!需要先校验一下格式
+    this.formRef1.current
+      .validateFields()
+      .then(value => {
+        console.log(this.state.initialValues);
+        this.setState({
+          initialValues: value,
+        });
+        const current = this.state.current + 1;
+        this.setState({ current });
+      })
+      .catch(err => {});
+  }
+
+  prev() {
+    const current = this.state.current - 1;
+    this.setState({ current });
+  }
+  handleClick() {
+    // this.formRef2.current
+    //   .validateFields()
+    //   .then(value => {
+    //     console.log(value);
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   });
   }
 }
 

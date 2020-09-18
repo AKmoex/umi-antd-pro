a52ec38a5cdf9979483fd34df9461041f510e27b
AKmoex <akmoex@gmail.com>
2020-09-18 18:41:09 +0800

    basic-form的list的ui

--- src/pages/list/basic-list.js
+++ src/pages/list/basic-list.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react';
 import BaseInfo from '../components/BaseInfo.js';
 
-import { Card, Row, Col, Radio, Input } from 'antd';
+import {
+  Card,
+  Row,
+  Col,
+  Radio,
+  Input,
+  Form,
+  Button,
+  Modal,
+  Select,
+  DatePicker,
+  List,
+  Avatar,
+} from 'antd';
 import '../../static/css/index.css';
+import { PlusOutlined } from '@ant-design/icons';
 
 class BasicList extends Component {
+  extraContent = (
+    <div>
+      <Radio.Group
+        onChange={this.handleStatusChange.bind(this)}
+        defaultValue="a"
+      >
+        <Radio.Button value="a">全部</Radio.Button>
+        <Radio.Button value="b">进行中</Radio.Button>
+        <Radio.Button value="c">等待中</Radio.Button>
+      </Radio.Group>
+      <Input.Search
+        placeholder="input search text"
+        onSearch={value => console.log(value)}
+        style={{ width: '300px', marginLeft: '20px' }}
+      />
+    </div>
+  );
+
+  data = [
+    {
+      title: 'Ant Design Title 1',
+    },
+    {
+      title: 'Ant Design Title 2',
+    },
+    {
+      title: 'Ant Design Title 3',
+    },
+    {
+      title: 'Ant Design Title 4',
+    },
+  ];
+  state = {
+    loading: false,
+    visible: false,
+  };
+
   render() {
+    const layout = {
+      labelCol: {
+        span: 6,
+      },
+      wrapperCol: {
+        span: 14,
+      },
+    };
     return (
       <div>
         <BaseInfo
@@ -39,28 +98,98 @@ class BasicList extends Component {
           </Row>
         </Card>
         <Card
-          title="你好"
+          title="基本列表"
           style={{ margin: '20px 20px' }}
-          extra={
-            <div>
-              <Radio.Group
-                onChange={this.handleStatusChange.bind(this)}
-                defaultValue="a"
-              >
-                <Radio.Button value="a">全部</Radio.Button>
-                <Radio.Button value="b">进行中</Radio.Button>
-                <Radio.Button value="c">等待中</Radio.Button>
-              </Radio.Group>
-              <Input.Search
-                placeholder="input search text"
-                onSearch={value => console.log(value)}
-                style={{ width: '300px', marginLeft: '20px' }}
-              />
-            </div>
-          }
+          extra={this.extraContent}
         >
-          <p>你噶</p>
-          <p>你</p>
+          <Button type="dashed" onClick={this.showModal} block>
+            <PlusOutlined /> 添加
+          </Button>
+          <Modal
+            visible={this.state.visible}
+            title="任务添加"
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+            footer={[
+              <Button key="back" onClick={this.handleCancel}>
+                Return
+              </Button>,
+              <Button
+                key="submit"
+                type="primary"
+                loading={this.state.loading}
+                onClick={this.handleOk}
+              >
+                Submit
+              </Button>,
+            ]}
+            width={650}
+          >
+            <Form {...layout}>
+              <Form.Item
+                name="taskName"
+                label="任务名称"
+                rules={[{ required: true, message: '请输入任务名称' }]}
+              >
+                <Input placeholder="请输入"></Input>
+              </Form.Item>
+              <Form.Item>
+                <Form.Item
+                  name="startTime"
+                  label="开始时间"
+                  rules={[
+                    {
+                      required: true,
+                      message: '请选择起止日期',
+                    },
+                  ]}
+                >
+                  <DatePicker showTime />
+                </Form.Item>
+              </Form.Item>
+              <Form.Item
+                name="taskLeader"
+                label="任务负责人"
+                rules={[{ required: true, message: '请选择任务负责人' }]}
+              >
+                <Select placeholder="请选择">
+                  <Select.Option value="zhouxiaoxiao">周晓晓</Select.Option>
+                  <Select.Option value="panmaomao">潘毛毛</Select.Option>
+                </Select>
+              </Form.Item>
+              <Form.Item
+                name="description"
+                label="产品描述"
+                rules={[
+                  {
+                    required: true,
+                    message: '请输入至少五个字符',
+                  },
+                ]}
+              >
+                <Input.TextArea placeholder="请输入至少五个字符" />
+              </Form.Item>
+            </Form>
+          </Modal>
+          <List
+            itemLayout="horizontal"
+            dataSource={this.data}
+            renderItem={item => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={
+                    <Avatar
+                      size={48}
+                      shape="square"
+                      src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                    />
+                  }
+                  title={<a href="https://ant.design">{item.title}</a>}
+                  description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                />
+              </List.Item>
+            )}
+          />
         </Card>
       </div>
     );
@@ -68,6 +197,21 @@ class BasicList extends Component {
   handleStatusChange() {
     console.log('你好');
   }
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+  handleOk = () => {
+    this.setState({ loading: true });
+    setTimeout(() => {
+      this.setState({ loading: false, visible: false });
+    }, 3000);
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
 }
 
 export default BasicList;
